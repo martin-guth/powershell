944402b3d49046b2680c2c05b71f3071e45e8171
martin-guth <martin_guth@hotmail.com>
2019-11-20 19:34:48 +0100

    Add files via upload

---
+++ testExcelPSLib.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module dbatools;
+Import-Module ExcelPSLib;
+
+$credential = Get-Credential;
+# get results from table 
+$result= Invoke-DbaQuery -SQLInstance localhost -Database master -Query "SELECT * FROM sys.tables" -SqlCredential $credential ;
+# debug the output
+$result | Out-GridView
+
+# export to excel
+$result | Export-OOXML -FileFullPath test.xlsx -WorksheetName test -AutoFit -HeaderStyle WhiteSmoke
